32e1ccf1ff5f911863b97218945cf6c9efacb2a9
KRP <kunal.r.rajpurohit@gmail.com>
2020-10-17 23:47:06 -0700

    Updates

---
+++ precache-manifest.28ec92a54a5ae1888356fbf281ad78d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a181e6ce1dc8f49979c150bf17956c",
+    "url": "/krp2/index.html"
+  },
+  {
+    "revision": "9c6ba1508ed0902a14e0",
+    "url": "/krp2/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1982219c7c0b3500ea26",
+    "url": "/krp2/static/css/main.7059a1ce.chunk.css"
+  },
+  {
+    "revision": "9c6ba1508ed0902a14e0",
+    "url": "/krp2/static/js/2.0fd701ea.chunk.js"
+  },
+  {
+    "revision": "33e2e8be73d60c7f078955edc9e11b66",
+    "url": "/krp2/static/js/2.0fd701ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1982219c7c0b3500ea26",
+    "url": "/krp2/static/js/main.a03388ac.chunk.js"
+  },
+  {
+    "revision": "0467c54b26a3b0572338",
+    "url": "/krp2/static/js/runtime-main.43c78ed6.js"
+  }
+]);
